b14fa05594181fbb24e83c6a447f5f4ffec9fa86
Chris <kimballc3@yahoo.com>
2022-07-28 16:32:20 -0500

    added db files

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username (pulled from .env file)
+    user: process.env.DB_USER,
+    // Your MySQL password (pulled from .env file)
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  },
+  console.log('Connected to the election database.')
+);
+
+module.exports = db;
