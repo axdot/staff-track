aa0b36b1dab802c0fcbe43d85d62212edb4c3c05
Chris <kimballc3@yahoo.com>
2022-07-28 17:05:41 -0500

    schema added

--- db/schema.sql
+++ db/schema.sql
@@ -1,47 +1,26 @@
-DROP TABLE IF EXISTS staff_track;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
 
 CREATE TABLE department (
-
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-
+  id: INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title: VARCHAR(30) NOT NULL,
+  salary: DECIMAL NOT NULL,
+  department_id: INTEGER,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(dep_id) ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
-
-);
-
-
-CREATE TABLE parties (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
-);
-
-CREATE TABLE candidates (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  party_id INTEGER,
-  industry_connected BOOLEAN NOT NULL,
-  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
-);
-
-CREATE TABLE voters (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE votes (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  voter_id INTEGER NOT NULL,
-  candidate_id INTEGER NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT uc_voter UNIQUE (voter_id),
-  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
-  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+CREATE TABLE employees (
+  id: INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name: VARCHAR(30) NOT NULL,
+  last_name: VARCHAR(30) NOT NULL,
+  role_id: INTEGER,
+  manager_id: INTEGER,
+      CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+      CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL
 );
